0b84cd215ff91ec5b401f928e68a58d449254185
Hardik Kumar . <hardikgugnani123@gmail.com>
2024-01-25 15:01:47 -0500

    first commit

---
+++ WebApplication2/Controllers/ProjectsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var projects = new List<Project>()
+            {
+                new Project { ProjectId = 1, Name = "Project 1", Description = "First Project" },
+                new Project { ProjectId = 2, Name = "Project 2", Description = "Second Project" }
+            };
+            return View(projects);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/Project.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+        [DataType(DataType.Date)]
+        public string Status { get; set; }
+
+
+    }
+}
